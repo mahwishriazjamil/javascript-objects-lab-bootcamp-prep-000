4b78a7a37e06d9d7ecf9e262c58eefb0d267899a
Mahwish Riaz-Jamil <mahwishcee@gmail.com>
2018-06-04 00:48:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   
   object[key] = value
-  return obj
+  return object
   
 }
